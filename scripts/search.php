6c95681983f30dfb26a2bb02ebd17cb18ef1f99b
djusab12 <djusab12@gmail.com>
2021-06-08 23:36:11 +0300

    created a search sign-ups page

---
+++ scripts/search.php
@@ -0,0 +1,92 @@
+<?php
+
+$FREE_MARKS = "https://youtu.be/dQw4w9WgXcQ";
+$sum = 2;
+$daud = "here";
+$kris = "here";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Search Sign-ups</title>
+  <link rel="stylesheet" href="../styles/search.css" />
+  <script src="https://kit.fontawesome.com/6ab0b12156.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<header>
+      <nav>
+        <ul>
+          <div>
+            <li>Slot-it[logo]</li>
+          </div>
+          <div>
+          <a href="../index.html"><li>Home</li></a>
+          <a href=""><li>Create</li></a>          
+          <a href="trentu.ca"><li>View</li></a>
+          <a href="derek.html"><li>My Account<i class="fa fa-user" aria-hidden="true"></i></li></a>
+        </div>
+        </ul>
+      </nav>      
+    </header>
+  <main>
+      <h1> Search for Sign-ups </h1>
+          <ul>
+              <li><input type="text" autocomplete="on" placeholder="Title..."/></li>
+              <li><button><i class="fa fa-search" aria-hidden="true"></i></button></li>
+          </ul>
+<h2>result for keyword "appointment"</h2>
+          <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th scope="col">Title</th>
+              <th scope="col">Date</th>
+              <th scope="col">Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Patient1 appointment</td>
+              <td>7th July 2021</td>
+              <td>12:30pm</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Patient2 appointment</td>
+              <td>8th august 2021</td>
+              <td>8:00am</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Patient3 appointment</td>
+              <td>2nd December 2021</td>
+              <td>10:00am</td>
+            </tr>
+
+            <tr>
+              <th scope="row">4</th>
+              <td>Patient4 appointment</td>
+              <td>20th December 2021</td>
+              <td>2:00pm</td>
+            </tr>
+          </tbody>
+        </table>
+  </main>
+  <footer>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="mailto:slot-it@gmail.com">Contact</a></li>
+        <li><i class="fas fa-phone-square-alt"></i> : +1(705)-123-1234</li>
+        <li>Slot-it[logo]</li>
+      </ul>
+      <p>&copy; 2021 - Slot-It - All rights reserved</p>
+      
+    </footer>
+</body>
