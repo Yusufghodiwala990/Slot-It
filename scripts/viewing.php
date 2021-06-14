d106f255f289702e862896ce9f14a942a3e92da1
djusab12 <djusab12@gmail.com>
2021-06-14 09:23:15 +0300

    created the viewing page and added the logo where necessary on other pages

--- scripts/viewing.php
+++ scripts/viewing.php
@@ -13,14 +13,15 @@ $kris = "here";
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Search Sign-ups</title>
-  <link rel="stylesheet" href="../styles/search.css" />
+  <title>Viewing Sign-ups</title>
+  <link rel="stylesheet" href="../styles/viewing.css" />
   <script src="https://kit.fontawesome.com/6ab0b12156.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
+
 <header>
-      <nav>
+<nav>
         <ul>
           <div>
             <li><img src="../img/logo.png" alt="Slot-it logo"></li>
@@ -32,61 +33,59 @@ $kris = "here";
           <a href="derek.html"><li>My Account<i class="fa fa-user" aria-hidden="true"></i></li></a>
         </div>
         </ul>
-      </nav>      
-    </header>
-  <main>
-      <h1> Search for Sign-ups </h1>
-          <ul>
-              <li><input type="text" autocomplete="on" placeholder="Title..."/></li>
-              <li><button><i class="fa fa-search" aria-hidden="true"></i></button></li>
-          </ul>
-<h2>result for keyword "appointment"</h2>
-          <table>
+      </nav>
+</header>
+
+<main>
+<h1> Some specific sign-up sheet<br> Owned by: Someone</h1>   
+    <table>
           <thead>
             <tr>
-              <th></th>
-              <th scope="col">Title</th>
-              <th scope="col">Date</th>
-              <th scope="col">Time</th>
+              <th scope="col">For-what</th>
+              <th scope="col">Scheduled-time</th>
+              <th scope="col">Who-has-signed-up</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row">1</th>
-              <td>Patient1 appointment</td>
-              <td>7th July 2021</td>
-              <td>12:30pm</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Patient2 appointment</td>
-              <td>8th august 2021</td>
-              <td>8:00am</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Patient3 appointment</td>
-              <td>2nd December 2021</td>
+              <td>Dentist appointment</td>
               <td>10:00am</td>
+              <td>Daud</td>
+            </tr>
+            <tr>
+              <td>Dentist appointment</td>
+              <td>10:40am</td>
+              <td><a href="">Slot-me-in</a></td>
+            </tr>
+            <tr>
+              <td>Dentist appointment</td>
+              <td>11:20am</td>
+              <td>Kris</td>
             </tr>
 
             <tr>
-              <th scope="row">4</th>
-              <td>Patient4 appointment</td>
-              <td>20th December 2021</td>
-              <td>2:00pm</td>
+              <td>Dentist appointment</td>
+              <td>12:00pm</td>
+              <td>Yusuf</td>
+            </tr>
+
+            <tr>
+              <td>Dentist appointment</td>
+              <td>10:40am</td>
+              <td><a href="">Slot-me-in</a></td>
             </tr>
           </tbody>
         </table>
-  </main>
-  <footer>
+</main>
+<footer>
       <ul>
         <li><a href="">Home</a></li>
         <li><a href="mailto:slot-it@gmail.com">Contact</a></li>
         <li><i class="fas fa-phone-square-alt"></i> : +1(705)-123-1234</li>
-        <li><img src="../img/logo.png" alt="Slot-it logo"></li>
+        <li> <img src="../img/logo.png" alt="Slot-it logo"></li>
       </ul>
       <p>&copy; 2021 - Slot-It - All rights reserved</p>
       
     </footer>
-</body>
+  </body>
+</html>
