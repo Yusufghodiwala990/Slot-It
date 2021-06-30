38adf3cc8993af5bbd46a22e3a2acaedb8a2cab4
djusab12 <djusab12@gmail.com>
2021-06-30 15:49:53 +0300

    made the php for mystuffview

--- scripts/mystuff.php
+++ scripts/mystuff.php
@@ -1,3 +1,21 @@
+<?php
+// $errors = array();   //declare empty array to add errors too
+session_start();
+$user=$_SESSION['user_id'];   //need this from yusuf's page
+include '../Includes/library.php';
+// CONNECT TO DATABASE
+$pdo = connectDB();
+$query = "select * from Signup_sheets where user_ID=?"; 
+$stmt = $pdo->prepare($query);
+$stmt->execute([$user]);
+$list1 = $stmt->fetchAll();
+
+$query1 = "select Signup_sheets.Title, Slots.Scheduled_slots from Signup_sheets INNER JOIN slots where user_ID=?"; 
+$stmt1 = $pdo->prepare($query1);
+$stmt1->execute([$user]);
+$lists2 = $stmt1->fetchAll();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -45,51 +63,19 @@
                     </tr>
                 </thead>
                 <tbody>
+                <?php foreach ($list1 as $row): ?>
                     <tr>
-                        <td>Dentist</td>
-                        <td>30</td>
-                        <td>17</td>
-                        <td> <a href="./viewing_owner.php" ><i class="fas fa-info-circle"> Details</i></a></td>
-                        <td> <a href="./edit_sheet.php"><i class="fas fa-edit"> Edit</i></a></td>
-                        <td> <a href="./edit_sheet.php"><i class="fas fa-trash"> Delete</i></a></td>
-                        <td> <a href="./copy.php"><i class="fas fa-copy"> Copy</i></a></td>
-                        <td> <a href="./copy.php"><i class="fa fa-link"> CopyURL</i></a></td>
-
-                    </tr>
-
-                    <tr>
-                        <td>Tennis</td>
-                        <td>22</td>
-                        <td>12</td>
-                        <td> <a href="./viewing_owner.php" ><i class="fas fa-info-circle"> Details</i></a></td>
-                        <td> <a href="./edit_sheet.php"><i class="fas fa-edit"> Edit</i></a></td>
-                        <td> <a href="./edit_sheet.php"><i class="fas fa-trash"> Delete</i></a></td>
-                        <td> <a href="./copy.php"><i class="fas fa-copy"> Copy</i></a></td>
-                        <td> <a href="./copy.php"><i class="fa fa-link"> CopyURL</i></a></td>
-
-                    </tr>
-
-                    <tr>
-                        <td>Soccer</td>
-                        <td>22</td>
-                        <td>12</td>
+                        <td><?=$row['Title']?></td>
+                        <td><?=$row['No_of_slots']?></td>
+                        <td><?=$row['no_of_signups']?></td>
                         <td> <a href="./viewing_owner.php" ><i class="fas fa-info-circle"> Details</i></a></td>
                         <td> <a href="./edit_sheet.php"><i class="fas fa-edit"> Edit</i></a></td>
                         <td> <a href="./edit_sheet.php"><i class="fas fa-trash"> Delete</i></a></td>
                         <td> <a href="./copy.php"><i class="fas fa-copy"> Copy</i></a></td>
                         <td> <a href="./copy.php"><i class="fa fa-link"> CopyURL</i></a></td>
                     </tr>
-                    <tr>
-                        <td>Labs</td>
-                        <td>22</td>
-                        <td>12</td>
-                        <td> <a href="./viewing_owner.php" ><i class="fas fa-info-circle"> Details</i></a></td>
-                        <td> <a href="./edit_sheet.php"><i class="fas fa-edit"> Edit</i></a></td>
-                        <td> <a href="./edit_sheet.php"><i class="fas fa-trash"> Delete</i></a></td>
-                        <td> <a href="./copy.php"><i class="fas fa-copy"> Copy</i></a></td>
-                        <td> <a href="./copy.php"><i class="fa fa-link"> CopyURL</i></a></td>
+                    <?php endforeach;?>
 
-                    </tr>
                 </tbody>
             </table>
 
@@ -99,26 +85,23 @@
                 <thead>
                     <tr>
                         <th scope="col">Title</th>
+                        <td>Date</td>
+                        <td>Time</td>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Dentist</td>
-                        <td> <a href="./viewing_user.php" ><i class="fas fa-info-circle"> Details</i></a></td>
-                        <td> <a href=""><i class="fas fa-window-close"> Cancel</i></a></td>
-                    </tr>
+                <?php foreach ($list2 as $row): ?>
 
                     <tr>
-                        <td>Football</td>
-                        <td> <a href="./viewing_user.php" ><i class="fas fa-info-circle"> Details</i></a></td>
-                        <td> <a href=""><i class="fas fa-window-close"> Cancel</i></a></td>
-                    </tr>
+                        <td><?=$row['Title']?></td>
+                        <td><?=$row['Scheduled_slot']?></td>
+                        <td><?=$row['Scheduled_slot']?></td>
 
-                    <tr>
-                        <td>Football</td>
                         <td> <a href="./viewing_user.php" ><i class="fas fa-info-circle"> Details</i></a></td>
                         <td> <a href=""><i class="fas fa-window-close"> Cancel</i></a></td>
                     </tr>
+                    <?php endforeach;?>
+
                 </tbody>
             </table>
     </main>
